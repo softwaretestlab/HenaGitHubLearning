f1085f8d9246813d0fd6d2673963e4369dfc635b
softwaretestlab <35885117+softwaretestlab@users.noreply.github.com>
2025-06-15 10:06:54 +1000

    Update AT01_Navigate.spec.ts

--- tests/AT01_Navigate.spec.ts
+++ tests/AT01_Navigate.spec.ts
@@ -7,3 +7,11 @@ test('has title', async ({ page }) => {
   await expect(page).toHaveTitle(/Playwright/);
   //new line added to test git commit
 });
+//jira-1234 automation test
+test('has titles', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Playwright/);
+  //new line added to test git commit
+});
